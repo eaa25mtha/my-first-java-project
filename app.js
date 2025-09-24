53a6759f3d00797de2db16c1b28a7bfb3df2f6ec
eaa25mtha <eaa25mtha@students.eaaa.dk>
2025-09-24 13:20:05 +0200

    små ændringer

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 document.body.innerHTML += "hello javascript";
 //alert("hello mulit media designers");
 console.log("hello multi media designere");
+
+//funktion når man trykker på knappen
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej" + document.queryselector("input").value;
+  document.querySelector("button").addEventListener("click", sayHello);
+}
