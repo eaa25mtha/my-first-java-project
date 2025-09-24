fd38b0ed8c6a4ce1308a85cb0e38bcd0e8afb470
eaa25mtha <eaa25mtha@students.eaaa.dk>
2025-09-24 14:20:02 +0200

    titel ændret og baggrund

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-document.body.innerHTML += "hello javascript";
+document.body.innerHTML;
 //alert("hello mulit media designers");
 console.log("hello multi media designere");
 
-//funktion når man trykker på knappen
+//funktion der siger hej "navn, alder og mail"
 function sayHello() {
-  document.querySelector("h3").textContent =
-    "Hej" + document.querySelector("input").value;
+  document.querySelector("h3").textContent = "Hej" + document.querySelector("#name").value + document.querySelector("#age").value
+  + document.querySelector("#mail").value;
 }
 
+//button
 document.querySelector("button").addEventListener("click", sayHello);
